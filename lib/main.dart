e70628221fccaa4364a9b4d5bf00ae07defdeb3e
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-11-30 13:32:31 +0200

    Update main.dart
    Changed the notification bar to be transparent

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+// Packages
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+// Files
 import 'my_app.dart';
 // import 'package:gap/gap.dart';
 
 void main() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, // Set status bar color to transparent
+    statusBarIconBrightness: Brightness.dark, // Use dark icons for status bar
+  ));
   runApp(const MyApp());
 }
