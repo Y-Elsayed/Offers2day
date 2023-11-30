479ed39b005b6b376edd07c175e946a7cd03ec74
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-11-30 11:58:45 +0200

    Update forget_password_page.dart
    - Changed the class from stateless to stateful widget
    - Added the 4 ellipsis
    - Added the text "Oh no! I forgot" and positioned it
    - Added the text field and the button to send the information

--- lib/forget_password_page.dart
+++ lib/forget_password_page.dart
@@ -1,15 +1,172 @@
+// Packages
 import 'package:flutter/material.dart';
 
-class ForgetPasswordScreen extends StatelessWidget {
+// Files
+import 'navigator_service.dart';
+
+class ForgetPasswordScreen extends StatefulWidget {
   const ForgetPasswordScreen({super.key});
 
   @override
+  State<ForgetPasswordScreen> createState() => _ForgetPasswordState();
+}
+
+class _ForgetPasswordState extends State<ForgetPasswordScreen> {
+  // Controllers
+  final TextEditingController _forgotController = TextEditingController();
+
+  // Navigators
+  NavigatorService navigatorService = NavigatorService();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: null,
-      body: Center(
-        child: Text('The Forget Password screen'),
-      ),
-    );
+    Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+        appBar: null,
+        body: Container(
+          width: 430,
+          height: 932,
+          clipBehavior: Clip.antiAlias,
+          decoration: const BoxDecoration(color: Color(0xFFF5F5F5)),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                top: 0,
+                child: Container(
+                  width: 430,
+                  height: 932,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: const BoxDecoration(color: Color(0xFFF5F5F5)),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: -98,
+                        top: -196,
+                        child: Container(
+                          width: 359,
+                          height: 359,
+                          decoration: const ShapeDecoration(
+                            color: Color.fromARGB(225, 255, 218, 185),
+                            shape: OvalBorder(),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 183,
+                        top: -376,
+                        child: Container(
+                          width: 466,
+                          height: 466,
+                          decoration: const ShapeDecoration(
+                            color: Color.fromARGB(175, 174, 217, 224),
+                            shape: OvalBorder(),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 368,
+                        top: 1266,
+                        child: Transform(
+                          transform: Matrix4.identity()
+                            ..translate(0.0, 0.0)
+                            ..rotateZ(3.14),
+                          child: Container(
+                            width: 466,
+                            height: 466,
+                            decoration: const ShapeDecoration(
+                              color: Color.fromARGB(175, 174, 217, 224),
+                              shape: OvalBorder(),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 636,
+                        top: 1067,
+                        child: Transform(
+                          transform: Matrix4.identity()
+                            ..translate(0.0, 0.0)
+                            ..rotateZ(3.14),
+                          child: Container(
+                            width: 359,
+                            height: 359,
+                            decoration: const ShapeDecoration(
+                              color: Color.fromARGB(225, 255, 218, 185),
+                              shape: OvalBorder(),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              const Positioned(
+                left: 34,
+                top: 276,
+                child: SizedBox(
+                  width: 256,
+                  height: 96,
+                  child: Text(
+                    'Oh no!\n          I forgot',
+                    style: TextStyle(
+                      color: Color(0xFF666666),
+                      fontSize: 32,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w700,
+                      height: 0,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                  left: 26,
+                  top: 414,
+                  child: SizedBox(
+                      width: screenSize.width * 0.9,
+                      height: 50,
+                      child: TextField(
+                        controller: _forgotController,
+                        decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.person,
+                                color: Color.fromARGB(175, 174, 217, 224)),
+                            hintText: '\t\tEnter your email / phone number',
+                            hintStyle: const TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w400),
+                            contentPadding:
+                                const EdgeInsets.symmetric(vertical: 0.0),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    width: 1,
+                                    color: Color.fromARGB(175, 174, 217, 224)),
+                                borderRadius: BorderRadius.circular(15.0))),
+                      ))),
+              Positioned(
+                  left: 70,
+                  top: 508,
+                  width: screenSize.width * 0.7,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Perform the logic for the reset pawssword
+
+                      navigatorService.popFromNavigator(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(175, 174, 217, 224)),
+                    child: const Text('Reset Password',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontFamily: 'Intel',
+                            fontWeight: FontWeight.w400)),
+                  ))
+            ],
+          ),
+        ));
   }
 }
