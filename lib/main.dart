151974329e1579fa85175c9637a533557e4cf173
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-12-02 21:32:45 +0200

    Deleted 'my_app.dart' & Updated 'main.dart'
    Took the contents of the 'my_app.dart' and placed it into 'main.dart' as to have a universal place for the contents of the app.
    
    Added routes for navigation, the routes are:
    - Login Page
    - Signup Page
    - Forgot Password Page
    - Home Page
    
    Removed the debug banner on the top right as it's annoying

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 // Files
-import 'my_app.dart';
-// import 'package:gap/gap.dart';
+import 'pages/login_page.dart';
+import 'pages/signup_page.dart';
+import 'pages/forget_password_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent, // Set status bar color to transparent
-    statusBarIconBrightness: Brightness.dark, // Use dark icons for status bar
-  ));
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+      routes: {
+        '/loginpage': (context) => const LoginPage(),
+        '/signuppage': (context) => const SignUpPage(),
+        '/forgetpasswordpage': (context) => const ForgetPasswordPage(),
+        '/homepage': (context) => const HomePage()
+      },
+    );
+  }
 }
