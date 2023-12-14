34cf827527dd956ab9969af819ecfbc592c9524f
Noor Tamer <noortamer@Noors-MacBook-Pro.local>
2023-12-14 23:23:42 +0200

    home page first version

--- lib/pages/home_pages/home.dart
+++ lib/pages/home_pages/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -11,62 +13,214 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
-    return Container(
-            width: screenSize.width,
-            height: screenSize.height,
-            clipBehavior: Clip.antiAlias,
-            decoration: const BoxDecoration(color: Color(0xFFF5F5F5)),
-            child: Column(children: [
-              const SearchBar(
-                hintText: "Search offers",
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xffF5F5F5),
+        body: Column(
+          children: [
+
+            SizedBox(
+              height: 40,
+            ),
+
+            Container(
+              color: Colors.transparent,
+
+              child: Row(
+
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+
+                  Container(
+                    height: 45,
+                    width: 287,
+                    decoration: BoxDecoration(
+                      color: Color(0xffAED9E0),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Row(
+                    
+                      children: [
+
+                        SizedBox(
+                          width: 10,
+                        ),
+                  
+                        Icon(Icons.search),
+                  
+                        SizedBox(
+                          width: 20,
+                        ),
+                  
+                        Text(
+                          "Search",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  
+
+                  GestureDetector(
+                    onTap: () {
+                      // hena hanrooh lel notifications page
+                      // Navigator.pushNamed(context, "/notifications");
+                    },
+                    child: Container(
+                      height: 53,
+                      width: 56,
+                      decoration: BoxDecoration(
+                        color: Color(0xffFFDAB9),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: Icon(
+                        Icons.notifications_none_sharp,
+                        size: 35,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              //starting from here will be a list view
-              Container(
-                  color: const Color.fromARGB(255, 248, 183, 108),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            ),
+      
+            SizedBox(
+              height: 60,
+            ),
+      
+            Container(
+              height: 187,
+              width: 432,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Color(0xffAED9E0),
+              ),
+              
+            ),
+
+            SizedBox(
+              height: 50,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+
+                Container(
+                  color: Colors.transparent,
+                  
+                  child: Column(
                     children: [
-                      IconButton(
-                          onPressed: () {
-                            //filter logic
-                          },
-                          icon: const Icon(Icons.restaurant_rounded, size: 40)),
-                      IconButton(
-                          onPressed: () {
-                            //filter logic
-                          },
-                          icon: const Icon(Icons.restaurant_rounded, size: 40)),
-                      IconButton(
-                          onPressed: () {
-                            //filter logic
-                          },
-                          icon: const Icon(Icons.restaurant_rounded, size: 40)),
+
+                      Container(
+                        height: 75,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Color(0xffFFDAB9),
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      
+                        child: Icon(
+                          Icons.restaurant_outlined,
+                          size: 30,
+                        ),
+                      ),
+
+                      Text(
+                        "Restaurants",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
                     ],
-                  )),
-              // const Gap(20),
-              //This will contain the Hot offers, automatically swiped
-              SizedBox(
-                width: screenSize.width,
-                height: screenSize.height * 0.4,
-                child: Image.asset(
-                  'assets/offer_placeholder.jpg',
-                  fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              //This will have the filter iconbuttons
-              //This will be the scrollable offers
-              Expanded(
-                child: SizedBox(
-                  width: screenSize.width,
-                  height: screenSize.height * 0.35,
-                  child: Container(
-                      color: Colors.blue,
-                      child: const Center(
-                          child: Text(
-                        "Here will be the user's feed",
-                      ))),
+
+                Container(
+                  color: Colors.transparent,
+                  
+                  child: Column(
+                    children: [
+
+                      Container(
+                        height: 75,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Color(0xffFFDAB9),
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      
+                        child: Icon(
+                          Icons.local_grocery_store_outlined,
+                          size: 35,
+                        ),
+                      ),
+
+                      Text(
+                        "Hyper Markets",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
                 ),
+
+                Container(
+                  color: Colors.transparent,
+                  
+                  child: Column(
+                    children: [
+
+                      Container(
+                        height: 75,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Color(0xffFFDAB9),
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      
+                        child: Icon(
+                          Icons.shopping_bag_outlined,
+                          size: 35,
+                        ),
+                      ),
+
+                      Text(
+                        "Clothing Stores",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+
+              ],
+            ),
+
+            SizedBox(
+              height: 50,
+            ),
+
+            Container(
+              height: 187,
+              width: 432,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Color(0xffAED9E0),
               ),
-            ]));
+              
+            ),
+
+
+          ],
+        ),
+      ),
+    );
   }
 }
