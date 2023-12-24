2135cb9b1af77081b377ae04ab3e16c4f23da334
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-12-24 16:20:58 +0200

    Update forget_password_page.dart
    Made the forget password page dynamic to any screen size

--- Frontend/lib/pages/forget_password_page.dart
+++ Frontend/lib/pages/forget_password_page.dart
@@ -16,154 +16,166 @@ class _ForgetPasswordState extends State<ForgetPasswordPage> {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: null,
-        body: Container(
-          width: 430,
-          height: 932,
-          clipBehavior: Clip.antiAlias,
-          decoration: const BoxDecoration(color: Color(0xFFF5F5F5)),
-          child: Stack(
-            children: [
-              Positioned(
-                left: 0,
-                top: 0,
-                child: Container(
-                  width: 430,
-                  height: 932,
-                  clipBehavior: Clip.antiAlias,
-                  decoration: const BoxDecoration(color: Color(0xFFF5F5F5)),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: -98,
-                        top: -196,
-                        child: Container(
-                          width: 359,
-                          height: 359,
-                          decoration: const ShapeDecoration(
-                            color: Color.fromARGB(225, 255, 218, 185),
-                            shape: OvalBorder(),
-                          ),
+      appBar: null,
+      body: Container(
+        width: screenSize.width,
+        height: screenSize.height,
+        clipBehavior: Clip.antiAlias,
+        decoration: const BoxDecoration(color: Color(0xFFF5F5F5)),
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              top: 0,
+              child: Container(
+                width: screenSize.width,
+                height: screenSize.height,
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(color: Color(0xFFF5F5F5)),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: -screenSize.width * 0.238,
+                      top: -screenSize.height * 0.225,
+                      child: Container(
+                        width: screenSize.width * 0.872,
+                        height: screenSize.height * 0.413,
+                        decoration: const ShapeDecoration(
+                          color: Color.fromARGB(225, 255, 218, 185),
+                          shape: OvalBorder(),
                         ),
                       ),
-                      Positioned(
-                        left: 183,
-                        top: -376,
+                    ),
+                    Positioned(
+                      left: screenSize.width * 0.444,
+                      top: -screenSize.height * 0.433,
+                      child: Container(
+                        width: screenSize.width * 1.132,
+                        height: screenSize.height * 0.537,
+                        decoration: const ShapeDecoration(
+                          color: Color.fromARGB(175, 174, 217, 224),
+                          shape: OvalBorder(),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: screenSize.width * 0.894,
+                      top: screenSize.height * 1.459,
+                      child: Transform(
+                        transform: Matrix4.identity()
+                          ..translate(0.0, 0.0)
+                          ..rotateZ(3.14),
                         child: Container(
-                          width: 466,
-                          height: 466,
+                          width: screenSize.width * 1.132,
+                          height: screenSize.height * 0.537,
                           decoration: const ShapeDecoration(
                             color: Color.fromARGB(175, 174, 217, 224),
                             shape: OvalBorder(),
                           ),
                         ),
                       ),
-                      Positioned(
-                        left: 368,
-                        top: 1266,
-                        child: Transform(
-                          transform: Matrix4.identity()
-                            ..translate(0.0, 0.0)
-                            ..rotateZ(3.14),
-                          child: Container(
-                            width: 466,
-                            height: 466,
-                            decoration: const ShapeDecoration(
-                              color: Color.fromARGB(175, 174, 217, 224),
-                              shape: OvalBorder(),
-                            ),
+                    ),
+                    Positioned(
+                      left: screenSize.width * 1.545,
+                      top: screenSize.height * 1.23,
+                      child: Transform(
+                        transform: Matrix4.identity()
+                          ..translate(0.0, 0.0)
+                          ..rotateZ(3.14),
+                        child: Container(
+                          width: screenSize.width * 0.872,
+                          height: screenSize.height * 0.413,
+                          decoration: const ShapeDecoration(
+                            color: Color.fromARGB(225, 255, 218, 185),
+                            shape: OvalBorder(),
                           ),
                         ),
                       ),
-                      Positioned(
-                        left: 636,
-                        top: 1067,
-                        child: Transform(
-                          transform: Matrix4.identity()
-                            ..translate(0.0, 0.0)
-                            ..rotateZ(3.14),
-                          child: Container(
-                            width: 359,
-                            height: 359,
-                            decoration: const ShapeDecoration(
-                              color: Color.fromARGB(225, 255, 218, 185),
-                              shape: OvalBorder(),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: screenSize.width * 0.082,
+              top: screenSize.height * 0.318,
+              child: SizedBox(
+                width: screenSize.width * 0.622,
+                height: 96,
+                child: const Text(
+                  'Oh no!\n          I forgot',
+                  style: TextStyle(
+                    color: Color(0xFF666666),
+                    fontSize: 32,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w700,
+                    height: 0,
                   ),
                 ),
               ),
-              const Positioned(
-                left: 34,
-                top: 276,
-                child: SizedBox(
-                  width: 256,
-                  height: 96,
-                  child: Text(
-                    'Oh no!\n          I forgot',
-                    style: TextStyle(
-                      color: Color(0xFF666666),
-                      fontSize: 32,
+            ),
+            Positioned(
+              left: screenSize.width * 0.063,
+              top: screenSize.height * 0.477,
+              child: SizedBox(
+                width: screenSize.width * 0.9,
+                height: 50,
+                child: TextField(
+                  controller: _forgotController,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(
+                      Icons.person,
+                      color: Color.fromARGB(175, 174, 217, 224),
+                    ),
+                    hintText: '\t\tEnter your email / phone number',
+                    hintStyle: const TextStyle(
+                      color: Colors.black45,
+                      fontSize: 15,
                       fontFamily: 'Inter',
-                      fontWeight: FontWeight.w700,
-                      height: 0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 11.0, horizontal: 16.0),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: Color.fromARGB(175, 174, 217, 224),
+                      ),
+                      borderRadius: BorderRadius.circular(15.0),
                     ),
                   ),
                 ),
               ),
-              Positioned(
-                  left: 26,
-                  top: 414,
-                  child: SizedBox(
-                      width: screenSize.width * 0.9,
-                      height: 50,
-                      child: TextField(
-                        controller: _forgotController,
-                        decoration: InputDecoration(
-                            prefixIcon: const Icon(Icons.person,
-                                color: Color.fromARGB(175, 174, 217, 224)),
-                            hintText: '\t\tEnter your email / phone number',
-                            hintStyle: const TextStyle(
-                                color: Colors.black45,
-                                fontSize: 15,
-                                fontFamily: 'Inter',
-                                fontWeight: FontWeight.w400),
-                            contentPadding: const EdgeInsets.symmetric(
-                                vertical: 11.0, horizontal: 16.0),
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(
-                                    width: 2,
-                                    color: Color.fromARGB(175, 174, 217, 224)),
-                                borderRadius: BorderRadius.circular(15.0))),
-                      ))),
-              Positioned(
-                  left: 70,
-                  top: 508,
-                  width: screenSize.width * 0.7,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Perform the logic for the reset pawssword
-                      Navigator.pop(context);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)
-                      ),
-                        backgroundColor:
-                            const Color.fromARGB(175, 174, 217, 224)),
-                    child: const Text('Reset Password',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontFamily: 'Intel',
-                            fontWeight: FontWeight.w400)),
-                  ))
-            ],
-          ),
-        ));
+            ),
+            Positioned(
+              left: screenSize.width * 0.17,
+              top: screenSize.height * 0.585,
+              width: screenSize.width * 0.7,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Perform the logic for the reset pawssword
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  backgroundColor: const Color.fromARGB(175, 174, 217, 224),
+                ),
+                child: const Text(
+                  'Reset Password',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontFamily: 'Intel',
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
