2de04f58c767cb6c2f001c34d5a5ca2e2591a9f6
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-12-25 21:30:07 +0200

    Update business_view.dart
    Fixed the positions for the widgets and fixed the error of outdated widget

--- Frontend/lib/pages/business_view.dart
+++ Frontend/lib/pages/business_view.dart
@@ -13,22 +13,22 @@ class _BusinessState extends State<Business> {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
-        width: 430,
-        height: 932,
+        width: screenSize.width,
+        height: screenSize.height,
         clipBehavior: Clip.antiAlias,
         decoration: const BoxDecoration(color: Color(0xFFF5F5F5)),
         child: Stack(
           children: [
             Positioned(
-              left: 250,
-              top: 1250,
+              left: screenSize.width * 0.607,
+              top: screenSize.height * 1.441,
               child: Transform(
                 transform: Matrix4.identity()
                   ..translate(0.0, 0.0)
                   ..rotateZ(3.14),
                 child: Container(
-                  width: 500,
-                  height: 500,
+                  width: screenSize.width * 1.215,
+                  height: screenSize.height * 0.576,
                   decoration: const BoxDecoration(
                     color: Color.fromARGB(175, 174, 217, 224),
                     shape: BoxShape.circle,
@@ -42,15 +42,15 @@ class _BusinessState extends State<Business> {
               ),
             ),
             Positioned(
-              left: 636,
-              top: 1067,
+              left: screenSize.width * 1.545,
+              top: screenSize.height * 1.23,
               child: Transform(
                 transform: Matrix4.identity()
                   ..translate(0.0, 0.0)
                   ..rotateZ(3.14),
                 child: Container(
-                  width: 359,
-                  height: 359,
+                  width: screenSize.width * 0.872,
+                  height: screenSize.height * 0.413,
                   decoration: const BoxDecoration(
                     color: Color.fromARGB(225, 255, 218, 185),
                     shape: BoxShape.circle,
@@ -58,11 +58,11 @@ class _BusinessState extends State<Business> {
                 ),
               ),
             ),
-            const Positioned(
-              top: 160,
+            Positioned(
+              top: screenSize.height * 0.155,
               left: 0,
               right: 0,
-              child: Center(
+              child: const Center(
                 child: Text(
                   'Business Profile',
                   style: TextStyle(
@@ -74,16 +74,10 @@ class _BusinessState extends State<Business> {
                 ),
               ),
             ),
-            const Positioned(
-              top: 210.0,
-              left: 105.0,
-              child: Stack(children: <Widget>[
-                CircleAvatar(
-                  radius: 110.0,
-                  backgroundImage:
-                      AssetImage('assets/images-2.png'),
-                ),
-              ]),
+            Positioned(
+              top: screenSize.height * 0.204,
+              left: screenSize.width * 0.229,
+              child: Image.asset("assets/images-2.png"),
             ),
             Positioned(
               top: screenSize.height * 0.42,
@@ -112,11 +106,15 @@ class _BusinessState extends State<Business> {
                       // Add your logic here
                     },
                     style: ElevatedButton.styleFrom(
-                      primary: Color(0xFFAED9E0),
-                      onPrimary: Colors.black,
-                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                      backgroundColor: const Color(0xFFAED9E0),
+                      foregroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 16,
+                      ),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50), // Changed the shape to a pill-shaped button
+                        borderRadius: BorderRadius.circular(
+                            50), // Changed the shape to a pill-shaped button
                       ),
                     ),
                     child: const Text(
@@ -134,9 +132,12 @@ class _BusinessState extends State<Business> {
                       // Add your logic here
                     },
                     style: ElevatedButton.styleFrom(
-                      primary: Color(0xffFFDAB9),
-                      onPrimary: Colors.black,
-                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                      backgroundColor: const Color(0xffFFDAB9),
+                      foregroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 16,
+                      ),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(50),
                       ),
